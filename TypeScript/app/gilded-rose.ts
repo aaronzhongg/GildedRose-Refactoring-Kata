8791ceee031187b205f1aff6d8f53703c4fcde63
Aaron Zhong <aaronzhongnz@gmail.com>
2024-02-12 16:54:41 +1300

    Extract methods, magic numbers

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -13,57 +13,73 @@ export class Item {
 export class GildedRose {
   items: Array<Item>;
 
+  private readonly MaximumQuality = 50;
+  private readonly MinimumQuality = 0;
+
   constructor(items = [] as Array<Item>) {
     this.items = items;
   }
 
   updateQuality() {
     this.items.forEach((item) => {
-      switch (item.name) {
-        case "Aged Brie":
-          if (item.quality >= 50) break;
+      this.updateItemQuality(item);
 
-          if (item.sellIn < 1) {
-            item.quality = item.quality + 2;
-          } else {
-            item.quality = item.quality + 1;
-          }
-
-          break;
-        case "Backstage passes to a TAFKAL80ETC concert":
-          if (item.sellIn < 1) {
-            item.quality = 0;
-            break;
-          }
-
-          if (item.quality == 50) break;
-
-          if (item.sellIn < 6) {
-            item.quality = item.quality + Math.min(3, 50 - item.quality);
-          } else if (item.sellIn < 11) {
-            item.quality = item.quality + Math.min(2, 50 - item.quality);
-          } else {
-            item.quality = item.quality + Math.min(1, 50 - item.quality);
-          }
-
-          break;
-        case "Sulfuras, Hand of Ragnaros":
-          break;
-        default: // Normal items
-          if (item.quality == 0) break;
-
-          if (item.sellIn > 0) {
-            item.quality = item.quality - 1;
-          } else {
-            item.quality = item.quality - Math.min(item.quality, 2);
-          }
-          break;
-      }
-
-      if (item.name != "Sulfuras, Hand of Ragnaros")
-        item.sellIn = item.sellIn - 1;
+      this.updateItemSellInDate(item);
     });
 
     return this.items;
+  }
+
+  private updateItemSellInDate(item: Item) {
+    if (item.name != "Sulfuras, Hand of Ragnaros")
+      item.sellIn = item.sellIn - 1;
+  }
+
+  private updateItemQuality(item: Item) {
+    switch (item.name) {
+      case "Aged Brie":
+        if (item.quality == this.MaximumQuality) break;
+
+        if (item.sellIn < 1) {
+          item.quality = item.quality + 2; // More aged brie is sought after, so quality increases at a faster rate after sellIn date passes
+        } else {
+          item.quality = item.quality + 1;
+        }
+
+        break;
+      case "Backstage passes to a TAFKAL80ETC concert":
+        // Concert has passed, there is no value in the ticket anymore
+        if (item.sellIn < 1) {
+          item.quality = this.MinimumQuality;
+          break;
+        }
+
+        if (item.quality == this.MaximumQuality) break;
+
+        // There is more demand in concert tickets as the concert date approaches, rate of quality increases accordingly
+        if (item.sellIn < 6) {
+          item.quality =
+            item.quality + Math.min(3, this.MaximumQuality - item.quality);
+        } else if (item.sellIn < 11) {
+          item.quality =
+            item.quality + Math.min(2, this.MaximumQuality - item.quality);
+        } else {
+          item.quality =
+            item.quality + Math.min(1, this.MaximumQuality - item.quality);
+        }
+
+        break;
+      case "Sulfuras, Hand of Ragnaros":
+        break;
+      default: // Normal items
+        if (item.quality == this.MinimumQuality) break;
+
+        if (item.sellIn > 0) {
+          item.quality = item.quality - 1;
+        } else {
+          item.quality = item.quality - Math.min(item.quality, 2);
+        }
+        break;
+    }
   }
 }
